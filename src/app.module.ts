afc4c0f6751cb069e9699e0e2ee1b8cacaf74612
Park Sang Yeop <81801012+lotus0204@users.noreply.github.com>
2023-03-19 02:37:32 +0900

    ✨email service config로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import emailConfig from './config/emailConfig';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, EmailModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [emailConfig],
+    }),
+    UsersModule,
+    EmailModule,
+  ],
   controllers: [],
   providers: [],
 })
